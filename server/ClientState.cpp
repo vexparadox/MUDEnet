37403334cd69c74a5fd06be11f54293573d9c216
William Meaton <will.meaton@gmail.com>
2018-03-17 17:11:45 +0000

    Added save/load of quests to client data

--- server/ClientState.cpp
+++ server/ClientState.cpp
@@ -1,11 +1,20 @@
 #include "ClientState.hpp"
-
+#include "Quest.hpp"
 ClientState::ClientState(json& client_obj) : m_inventory(client_obj["inventory"])
 {
 	m_id = client_obj["id"];
 	m_username = client_obj["username"];
 	m_password = client_obj["password"];
 	m_locationid = client_obj["location"];
+    for(auto quest : client_obj["active_quests"])
+    {
+        m_active_quests.push_back(quest);
+    }
+
+    for(auto quest : client_obj["completed_quests"])
+    {
+        m_completed_quests.push_back(quest);
+    }
 }
 
 void ClientState::save(json& client_obj) const
@@ -17,4 +26,16 @@ void ClientState::save(json& client_obj) const
     json inv_obj;
     m_inventory.save(inv_obj);
     client_obj["inventory"] = inv_obj;
+    client_obj["active_quests"] = m_active_quests;
+    client_obj["completed_quests"] = m_completed_quests;
+}
+
+bool ClientState::has_completed_quest(int quest_id) const
+{
+    return std::find(m_completed_quests.begin(), m_completed_quests.end(), quest_id) != m_completed_quests.end();
+}
+
+bool ClientState::has_completed_quest(const Quest& quest) const
+{
+    return has_completed_quest(quest.ID());
 }
--- server/ClientState.hpp
+++ server/ClientState.hpp
@@ -4,6 +4,7 @@
 #include "Inventory.hpp"
 
 using json = nlohmann::json;
+class Quest;
 class ClientState
 {
 private:
@@ -13,6 +14,9 @@ private:
 	int m_locationid = 0;
 
 	Inventory m_inventory;
+
+	std::vector<int> m_completed_quests;
+	std::vector<int> m_active_quests;
 
 	ENetPeer* m_enet_peer = nullptr; // set only when the user is connected
 public:
@@ -27,6 +31,8 @@ public:
 	const Inventory& inventory() const { return m_inventory; }
 	const std::string& username() const { return m_username; }
 	const std::string& password() const { return m_password; }
+	bool has_completed_quest(int quest_id) const;
+	bool has_completed_quest(const Quest&) const;
 	int ID() const { return m_id; }
 	int location_id() const {return m_locationid;}
 	void set_location(int l) { m_locationid = l;}
